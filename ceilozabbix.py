cace795a86023f380a195a18349230b4dfb8aea3
XIEKE <xie_ke_ok@126.com>
2018-07-05 16:51:12 +0800

    Update ceilozabbix.py

--- ceilozabbix.py
+++ ceilozabbix.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
-# 定制数据发送到zabbix
+# 功能：初始化环境，加载配置文件，启动服务
 import cotyledon
 from oslo_config import cfg
 
-from ceilometer import zabbix_sender
+from ceilometer import custom_zabbix_sender
 from ceilometer import service
 
 CONF = cfg.CONF
@@ -12,5 +12,5 @@ CONF = cfg.CONF
 def main():
     service.prepare_service()
     sm = cotyledon.ServiceManager()
-    sm.add(zabbix_sender.ZabbixSenderService, workers=CONF.ceilozabbix.workers)
+    sm.add(custom_zabbix_sender.ZabbixSenderService, workers=CONF.ceilozabbix.workers)
     sm.run()
